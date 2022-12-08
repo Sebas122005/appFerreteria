941ed8bd4f13013a37dd53a64373fd7871a670f2
andyfgc <gonzalescolmenaresa@gmail.com>
2022-12-08 09:50:24 -0500

    actualizado
    1

---
+++ app/src/main/java/com/example/app/poo/java/appferreteria/view/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.app.poo.java.appferreteria.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.app.poo.java.appferreteria.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/app/poo/java/appferreteria/view/RegistrarUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.example.app.poo.java.appferreteria.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.app.poo.java.appferreteria.R;
+
+
+import android.os.Bundle;
+
+public class RegistrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+}
